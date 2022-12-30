bdfa9cfb6917c2299e105739b3d1deab13355433
Joakim Andersson <joakim@ktv-sjobo.se>
2022-12-30 12:38:16 +0100

    Adds the header and footer components. And css

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,7 @@
 export default function Header() {
     return (
-        <h1>Keeper app</h1>
+        <header>
+           <h1>Keeper app</h1>
+        </header>
     )
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,8 @@ export default function Home() {
       <main className={styles.main}>
 
       </main>
+
+      <Footer></Footer>
       
     </>
   )
